e4f6a8e1fdc870c4d9ee02f4f0c5beab205713dc
BENDADDA BENAOUDA <94899619+BENDADDA@users.noreply.github.com>
2023-01-06 00:20:53 -0800

    first commit

---
+++ MandelbrotSet/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MandelbrotSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw Mandelbort Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(275, 421);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0.285+0i";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(159, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Draw Julia Set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(275, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "inter a compex number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 458);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Created by BENAOUDA BENDADDA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MandelbrotSet/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Matlab;
+namespace MandelbrotSet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+        }
+        private double map(double value, double start, double end, double a, double b)
+        {
+            return (value - start) * (b - a) / (end - start) + a;
+        }
+        private void drowMandelbort(int iter,int n)
+        {
+            double minZome = -2,MaxZome=2;
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+            Bitmap bitmap = new Bitmap(w, h);
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    double x = map(i, 0, w, minZome, MaxZome);
+                    double y = map(j, 0, h, minZome, MaxZome);
+                    Complex c = new Complex(x, y);
+                    Complex z = 0;
+                    int k = 0;
+                    do
+                    {
+                        z = z*z + c;
+                        if (z.ABS > 2) break;
+                        k++;
+                    } while (k < iter);
+                    Color co1 = Color.Black;
+                    double hu = Math.Sqrt(k * 1.0 / iter);
+                    if (iter != k) co1 = SuperColor.FromAhsl(hu * 100);
+                    bitmap.SetPixel(i, j, co1);
+
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+        private void drawJuliaSet(int iter, Complex c)
+        {
+            double minZome = -2, MaxZome = 2;
+            int w = pictureBox1.Width;
+            int h = pictureBox1.Height;
+            Bitmap bitmap = new Bitmap(w, h);
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    double x = map(i, 0, w, minZome, MaxZome);
+                    double y = map(j, 0, h, minZome, MaxZome);
+                    Complex z = new Complex(x,y);
+                    int k = 0;
+                    do
+                    {
+                        z = z * z + c;
+                        if (z.ABS > MaxZome-minZome) break;
+                        k++;
+                    } while (k < iter);
+                    Color co1 = Color.Black;
+                    double hu=Math.Sqrt(k*1.0/iter);
+                    if (iter != k) co1 = SuperColor.FromAhsl(hu * 100);
+                    bitmap.SetPixel(i, j, co1);
+
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drowMandelbort(100,2);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Complex c = Complex.Parse(textBox1.Text);
+                drawJuliaSet(100, c);
+            }
+            catch
+            { }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //double x = map(Form1.MousePosition.X, 0, Width, -1, 1);
+            //double y = map(Form1.MousePosition.Y, 0, Height, -1, 1);
+            //Complex c = new Complex(x, y);
+            //drawJuliaSet(100, c);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //double angel = 0;
+            //while (true)
+            //{
+            //    Complex c = Maths.Cos(angel);
+            //    drawJuliaSet(100, c);
+            //    angel+=0.02;
+            //}
+
+        }
+
+      
+    }
+}
